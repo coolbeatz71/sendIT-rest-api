6e1ee106cb2024730c688de88980979155a61d63
coolbeatz71 <sigmacool@gmail.com>
2018-11-09 17:07:44 +0200

    Creating the middleware to protect important routes [finishes #161779992]

---
+++ src/authMiddleware.js
@@ -0,0 +1,30 @@
+// importing models
+import User from './models/user';
+
+const checkAuth = (request, response, next) => {
+  if (!request.headers.authorization || request.headers.authorization.indexOf('Bearer ') === -1) {
+    response.status(401).json({
+      error: true,
+      authKeyMissed: true,
+    });
+  } else {
+    // split the header value to get only teh authKey (Bearer wuyhdu3Y488478Eehjh...)
+    const authKey = request.headers.authorization.split(' ')[1];
+
+    // verify the authKey
+    const user = new User();
+    const isTokenValid = user.isTokenValid(authKey);
+
+    if (!isTokenValid) {
+      response.status(401).json({
+        error: true,
+        authKeyInvalid: true,
+      });
+    } else {
+      const userId = user.getUserIdByToken(authKey);
+      next();
+    }
+  }
+};
+
+module.exports = checkAuth;
--- src/routes/parcel.js
+++ src/routes/parcel.js
@@ -1,12 +1,17 @@
 import express from 'express';
 
+// importing models
+import Parcel from '../models/parcel';
+import User from '../models/user';
+import checkAuth from '../authMiddleware';
+
 const router = express.Router();
 
 /**
  * route to fetch all parcels delivery orders
  * @method GET
  */
-router.get('/', (request, response, next) => {
+router.get('/', (request, response) => {
   response.status(200).json({
     message: 'get request to /parcels',
   });
@@ -16,17 +21,25 @@ router.get('/', (request, response, next) => {
  * route to create a parcel delivery order
  * @method POST
  */
-router.post('/', (request, response, next) => {
-  response.status(200).json({
-    message: 'post request to /parcels',
-  });
+router.post('/', checkAuth, (request, response) => {
+  // get sign data from the request body
+  const {
+    parcelName, description, pickupLocation, destination, weight,
+  } = request.body;
+
+  // We should get the userId using the authKey in the header
+  const authKey = request.headers.authorization.split(' ')[1];
+
+  const user = new User();
+  const userId = user.getUserIdByToken(authKey);
+  console.log(userId);
 });
 
 /**
  * route to fetch a specific delivery order by its ID
  * @method GET
  */
-router.get('/:parcelId', (request, response, next) => {
+router.get('/:parcelId', (request, response) => {
   const { parcelId } = request.params;
   response.status(200).json({
     message: 'get request to /parcels for an individual parcel order',
